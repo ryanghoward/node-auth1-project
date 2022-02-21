72b4093b3bdc84b3bfa0180a520d45d53945be1a
Ryan Howard <ryanghoward@protonmail.com>
2022-02-21 14:36:44 -0800

    commit

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,11 @@
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
 const router = require("express").Router();
+const {
+  checkPasswordLength,
+  checkUsernameExists,
+  checkUsernameFree,
+} = require("./auth-middleware");
 
 /**
   1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
@@ -25,9 +30,14 @@ const router = require("express").Router();
   }
  */
 
-router.post("/register", (req, res, next) => {
-  res.json("register");
-});
+router.post(
+  "/register",
+  checkPasswordLength,
+  checkUsernameFree,
+  (req, res, next) => {
+    res.json("register");
+  }
+);
 
 /**
   2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
@@ -45,7 +55,7 @@ router.post("/register", (req, res, next) => {
   }
  */
 
-router.post("/login", (req, res, next) => {
+router.post("/login", checkUsernameExists, (req, res, next) => {
   res.json("login");
 });
 
